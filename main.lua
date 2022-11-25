a4494b0e92f4facd41338f04418edcc339f59ed4
Paul Taylor <pltorigami@gmail.com>
2022-11-25 12:15:20 -0500

    Add frame advancing functionality for algorithm animation

--- main.lua
+++ main.lua
@@ -8,25 +8,28 @@ function love.load()
 	GridLeft = 100
 	GridTop = 100
 	GridSize = 15
+	FramesPerSec = 5
 
-	SourceSet = false
-	DestSet = false
+	ElapsedTime = 0
+	PrevFrameInt = 0
+	Source = nil
+	Destination = nil
 	Grid = utils.makeGrid(GridSize)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
 	-- shift + click should be used to set source and destination, but regular click can
 	-- be used to make a wall
-	if utils.mouseInGrid(x, y, GridLeft, GridTop, GridSize, CellSize) then
-		local row = math.floor((y - GridTop) / CellSize) + 1
-		local column = math.floor((x - GridLeft) / CellSize) + 1
+	if utils.mouseInGrid(x, y, GridLeft, GridTop, GridSize, CellSize) and Destination == nil then
+		local row = math.ceil((y - GridTop) / CellSize)
+		local column = math.ceil((x - GridLeft) / CellSize)
 		if love.keyboard.isDown("rshift", "lshift") then
-			if not SourceSet then
+			if Source == nil then
 				Grid[row][column] = 2
-				SourceSet = true
+				Source = { row, column }
 			else
 				Grid[row][column] = 3
-				DestSet = true
+				Destination = { row, column }
 			end
 		else
 			Grid[row][column] = 1
@@ -36,12 +39,22 @@ end
 
 function love.mousemoved(x, y, dx, dy, istouch)
 	-- dragging mouse creates walls
-	if utils.mouseInGrid(x, y, GridLeft, GridTop, GridSize, CellSize) then
+	if utils.mouseInGrid(x, y, GridLeft, GridTop, GridSize, CellSize) and Destination == nil then
 		if love.mouse.isDown(1) then
 			local row = math.floor((y - GridTop) / CellSize) + 1
 			local column = math.floor((x - GridLeft) / CellSize) + 1
 			Grid[row][column] = 1
 		end
+	end
+end
+
+function love.update(dt)
+	ElapsedTime = ElapsedTime + dt
+	if math.floor(FramesPerSec * ElapsedTime) ~= PrevFrameInt then
+		PrevFrameInt = math.floor(FramesPerSec * ElapsedTime)
+		-- advance frame of algorithm here
+		-- should record current cell and have some function that can advance algorithm
+		-- one frame based on that cell, etc.
 	end
 end
 
